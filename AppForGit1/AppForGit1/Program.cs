3773d1526b82cee37373bdd071dd813b1d886960
DenLysko <denislysko@yandex.ru>
2022-07-13 01:21:17 +0300

    Initial Version

---
+++ AppForGit1/AppForGit1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
